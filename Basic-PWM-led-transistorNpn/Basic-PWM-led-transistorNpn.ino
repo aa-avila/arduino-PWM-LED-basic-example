f09b9310f1145535e8b6ff7e1a81acd9b16841d0
Agustin Avila <agualvila@gmail.com>
2021-03-24 21:23:41 -0300

    first commit

---
+++ Basic-PWM-led-transistorNpn/Basic-PWM-led-transistorNpn.ino
@@ -0,0 +1,31 @@
+const int LED_PIN = 3; //Elegimos un pin con PWM
+
+int delayTime = 1000; //tiempo de delay en milisegundos
+int loopTime = 10;
+
+void setup() {
+  //Seteamos pin
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  efectoSubida();
+  efectoBajada();
+}
+
+///////////////////////////////////////////////////////////
+void efectoSubida() {
+  for (int counter = 0; counter < 255; counter++) {
+    analogWrite(LED_PIN, counter);
+    delay(loopTime);
+  }
+  delay(delayTime);
+}
+
+void efectoBajada() {
+  for (int counter = 255; counter >= 0; counter--) {
+    analogWrite(LED_PIN, counter);
+    delay(loopTime);
+  }
+  delay(delayTime);
+}
